a61f0b2ac236d42326e43e25f152b085abd0fa6f
Bryan Eduardo <bryan@DESKTOP-AI0HI6U.>
2025-02-08 12:05:20 -0600

    fix: Fixed oldworld colorscheme and removed unnecesary colorschemes.

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -16,55 +16,14 @@ return {
     end,
   },
   {
-    "CosecSecCot/midnight-desert.nvim",
-    dependencies = {
-      "rktjmp/lush.nvim",
-    },
-    name = "midnight-desert",
-    config = function()
-      -- vim.cmd.colorscheme("midnight-desert")
-    end,
-  },
-  {
-    "water-sucks/darkrose.nvim",
-    name = "darkrose",
-    config = function()
-      -- vim.cmd.colorscheme("darkrose")
-    end,
-  },
-  {
-    "Shadorain/shadotheme",
-    name = "shadotheme",
-    config = function()
-      -- vim.cmd.colorscheme("shadotheme")
-    end,
-  },
-  {
-    "p00f/alabaster.nvim",
-    name = "alabaster",
-    config = function()
-      -- vim.cmd.colorscheme("alabaster")
-    end,
-  },
-  {
-    "yorumicolors/yorumi.nvim",
-    name = "yorumi",
-    config = function()
-      -- vim.cmd.colorscheme("yorumi")
-    end,
-  },
-  {
-    "miikanissi/modus-themes.nvim",
-    name = "modus-themes",
-    config = function()
-      -- vim.cmd.colorscheme("modus-vivendi")
-    end,
-  },
-  {
     "dgox16/oldworld.nvim",
-    lazy = false,
     priority = 1000,
+    lazy = false,
+    name = "oldworld",
     config = function()
+      require("oldworld").setup({
+        variant = "cooler",
+      })
       vim.cmd.colorscheme("oldworld")
     end,
   },
